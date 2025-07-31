91504cbdaefe0a9f3b731bede6380928e8b89dae
LuisGon23 <gonzalezmluis08@gmail.com>
2025-07-31 12:26:26 -0500

    repositorio js versión 1

---
+++ 1_sintaxis_variables/app.js
@@ -0,0 +1,33 @@
+
+//El alert muestra una ventana emergente en el navegador
+alert ("Hola Mundo");
+//Prompt muestra una caja para ingresar informmac
+prompt ("Dime tu nombre");
+
+//Variable declarada con let
+
+let personaje = "Turbo"
+
+//Crear variable puntaje y asignar valor 0
+let puntaje = 0
+//Crear variable shots y asignar valor 5
+let shots = 3
+    recarga = 2
+
+
+console.log(" Total balas:",shots + recarga)    
+
+
+//El usuario capturo 4 patos 
+puntaje += 4
+//El usuario gasto 2 balas
+shots -= 2
+//El usuario recargo su arma
+recarga += 1
+recarga++ // ++ permite sumar de a uno
+
+
+console.log("Puntaje:",puntaje)
+console.log("shots:", shots)
+console.log("recarga:",recarga)
+
---
+++ 2_operadores_comparacion_logicos/app.js
@@ -0,0 +1,28 @@
+//Boolean (...)
+
+console.log('"5" === "5" ', "5" =="5") //true
+console.log('"5" === "5" ', "5" === "5") //true
+console.log('"5" === "5" ', 5 === "5") //false
+console.log('"5" === "5" ', 5 === "5") //false
+
+
+//Concatenar  (+)
+
+console.log( 5 + "3") // 53
+console.log( 5 * "3") // 15
+console.log( 5 + 3) // 8
+
+
+    //Pida dos numeros al usuario y sumelos
+
+    let num1= Number (prompt ('Dame un num1'))
+    let num2= Number (prompt ('Dame un num2') )
+    let resultado = num1 + num2
+    alert ("El resultado de tu suma es " + resultado)
+
+    //Ejercicio
+    
+    let num1= prompt ('Dame un numero')
+    let num2= prompt ('Dame un numero')
+
+    console.log('"A" ===',"A" === "A" )
