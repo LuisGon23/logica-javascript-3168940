b63e6a80f741ac957fdd40bdac238265b6339cc8
LuisGon23 <gonzalezmluis08@gmail.com>
2025-08-04 13:33:32 -0500

    Condicionales adelanto

--- app.js
+++ app.js
@@ -1,47 +1,20 @@
-//Boolean (...)
+prompt ('Dame tu nombre')
 
-console.log('"5" === "5" ', "5" =="5") //true
-console.log('"5" === "5" ', "5" === "5") //true
-console.log('"5" === "5" ', 5 === "5") //false
-console.log('"5" === "5" ', 5 === "5") //false
-
-
-//Concatenar  (+)
-
-console.log( 5 + "3") // 53
-console.log( 5 * "3") // 15
-console.log( 5 + 3) // 8
-
-
-    //Pida dos numeros al usuario y sumelos
-
-    let num1= Number (prompt('Dame un num1'))
-    let num2= Number (prompt('Dame un num2') )
-    let resultado = num1 + num2
-    alert ("El resultado de tu suma es " + resultado)
-
-    //Ejercicio
-    
-   let numero1 = Number (prompt ( "Dame un numero" + numero1))
-   let numero2 = Number (prompt ( "Dame un numero" + numero2))
-
-   console.log ("Su valor A es:" + numero1)
-   console.log ("Su valor B es:" + numero2)
-/* 
-    let suma= numero1 + numero2
-    console.log("El resultado de tu Suma:"+ numero1+ numero2 + suma)
-
-    let resta= numero1 - numero2
-    console.log("El resultado de tu Resta"+ numero1 + numero2 +resta)
+var nombre = "Carlos";
+    alert ("Aun no has dicho tu nombre bro")
+    if(nombre == "Carlos") { 
+alert ("No eres Carlos")
+    } else {    
+        alert ("Hemos guardado tu nombre")
+    }
+prompt ('Dame tu edad')
+var edad = 18;
+if (edad >= 18) {   
+    alert ("Eres mayor de edad")
+} 
 
 
-    
-    let Multiplicacion= numero1 - numero2
-    console.log("El resultado de tu Multiplicacion"+ multiplicacion)
-
-    let Division= numero1 - numero2
-    console.log("Division"+ division) */
-
-    //Solucion del ejercicio concatenado
-
-    console.log(`Tu numero es ${numerin1}`)
+if  (edad <17) {    
+} else {    
+    alert ("Eres menor de edad")
+}
