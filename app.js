afa2235738e9eceeca2489803e5be70f89b81ba0
LuisGon23 <gonzalezmluis08@gmail.com>
2025-08-06 12:35:45 -0500

    3_Condicionales terminado

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
-prompt ('Dame tu nombre')
+//arrays
+let frutas = ["mango", "piña", "papaya", "uva","limón","aguacate"]
 
-var nombre = "Carlos";
-    alert ("Aun no has dicho tu nombre bro")
-    if(nombre == "Carlos") { 
-alert ("No eres Carlos")
-    } else {    
-        alert ("Hemos guardado tu nombre")
-    }
-prompt ('Dame tu edad')
-var edad = 18;
-if (edad >= 18) {   
-    alert ("Eres mayor de edad")
-} 
-
-
-if  (edad <17) {    
-} else {    
-    alert ("Eres menor de edad")
+//objetos
+let pelicula = {    
+    nombre: "Back to the future",
+    estreno: 1986,
+    director: "Robert Zemeckis",
+    calificacion: "8.5"
 }
