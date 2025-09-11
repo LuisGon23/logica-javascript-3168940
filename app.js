6ce61fdefefbca65e3181d129104beab4aa2258f
LuisGon23 <gonzalezmluis08@gmail.com>
2025-09-11 11:28:27 -0500

    Doom contador

--- app.js
+++ app.js
@@ -1,40 +1,37 @@
-const formulario = document.querySelector(".form")
-const inputNombre = document.querySelector(".inputNombre")
-const listaColores = document.querySelector("#color")
-const userPage = document.querySelector(".userPage")
-const imgEquipo = document.querySelector(".img-equipo")
-const textSaludo = document.querySelector(".text-saludo")
-const textEquipo = document.querySelector(".text-equipo")
+//Seleccionar los elementos con los que va a interactuar o los que responden a la interacción
 
-const traducirColor = (color) => {
-  const colores = {
-    Rojo: "red",
-    Verde: "green",
-    Naranja: "orange"
-  }
-  return colores[color] || color 
+const pantalla = document.querySelector(".pantalla")
+const btnMenos = document.querySelector(".btn-menos")
+const btnMas = document.querySelector(".btn-mas")
+const body = document.body;
+let counter= 0
+
+//2. Función
+
+function incrementar () {   
+    counter++ 
+    pantalla.textContent = counter
+
+    if (counter === 10) { 
+        pantalla.style.color = "red"
+    }
+    if (counter === 30) {
+        body.style.backgroundColor = "yellow";
+    }
 }
 
-const enviarData = (e) => {
-  e.preventDefault()
-
-  const equipo = document.querySelector('input[name="equipo"]:checked')
-
-  const userInfo = {
-    nombre: inputNombre.value,
-    color: listaColores.value,
-    equipo: equipo.value
-  }
-
-  console.log(userInfo)
-
-  document.body.style.background = traducirColor(userInfo.color)
-
-  textSaludo.textContent = `Hola ${userInfo.nombre}`
-  textEquipo.textContent = userInfo.equipo
-
-  userPage.style.display = "block"
-  formulario.style.display = "none"
+function decrementar () {   
+    if (counter > 0) {
+        counter--
+        if (counter < 10) { 
+            pantalla.style.color = "black"
+        }
+        pantalla.textContent = counter
+    }
 }
 
-formulario.addEventListener("submit", enviarData)
+
+//3. Crear el evento
+
+btnMas.addEventListener("click", incrementar)
+btnMenos.addEventListener("click", decrementar)
